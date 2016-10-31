f42d4b67f07a39888949cf6201e255bc10551eb8
kobikalif <kobi.kalif@ironsrc.com>
2016-10-31 16:57:57 +0200

    move back to normal

--- simple_example/memory_analysis.py
+++ simple_example/memory_analysis.py
@@ -6,8 +6,6 @@ def foo():
     a = [1] * (10 ** 6)
     b = [2] * (2 * 10 ** 7)
     # add dome lines here and push it to your branch
-    # this is my change
-    # this is my change 2
     del b
     return a
 
