15247756eb84d7d48aae5c8f769f8d37b04b4963
kobikalif <kobi.kalif@ironsrc.com>
2016-10-31 14:08:39 +0200

    init code

---
+++ simple_example/memory_analysis.py
@@ -0,0 +1,13 @@
+from memory_profiler import profile
+
+
+@profile
+def foo():
+    a = [1] * (10 ** 6)
+    b = [2] * (2 * 10 ** 7)
+    # add dome lines here and push it to your branch
+    del b
+    return a
+
+if __name__ == '__main__':
+    foo()
